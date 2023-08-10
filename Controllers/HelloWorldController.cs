d2a5787de5aa0432f220c489e2b66ddfc9a748cd
Rodrigo Valle <fernandovalle939@gmail.com>
2023-08-10 11:59:50 -0600

    Se agregó el controlador HelloWorld y los modelos Pelicula y Persona

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPeliculas.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //
+        // GET: /HelloWorld/Welcome/
+        public ActionResult Welcome(string nombre, string apellido, int numVeces=1)
+        {
+            ViewBag.nombre = nombre;
+            ViewBag.apellido = apellido;
+            ViewBag.NumVeces = numVeces;
+            ViewBag.Mensaje = "Hola " + nombre + " " + apellido + ", veces mostrado: ";
+            return View();
+        }
+    }
+}
---
+++ Models/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCPeliculas.Models
+{
+    public class Persona
+    {
+        public string DUI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Direccion { get; set; }
+        public string Email { get; set; }
+    }
+}
